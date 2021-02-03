eee50cc39106ed96f1885d2d4821a003347f9a85
Thiago Alves <thiago_marketingdigital@hotmail.com>
2021-02-03 02:17:18 -0300

    created domain entities

---
+++ domain/model/account.go
@@ -0,0 +1,43 @@
+package model
+
+import (
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+	"time"
+)
+
+type Account struct {
+	Base      `valid:"required"`
+	OwnerName string    `json:"owner_name" gorm:"column:owner_name;type:varchar(255);not null" valid:"notnull"`
+	Bank      *Bank     `valid:"-"`
+	BankID    string    `gorm:"column:bank_id";type:uuid;not null" valid:"-"`
+	Number    string    `json:"number" gorm:"column:number;type:varchar(20)" valid:"notnull"`
+	PixKeys   []*PixKey `gorm:"ForeignKey:AccountID" valid:"-"`
+}
+
+func (account *Account) isValid() error {
+	_, err := govalidator.ValidateStruct(account)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewAccount(bank *Bank, number string, ownerName string) (*Account, error) {
+	account := Account{
+		OwnerName: number,
+		Bank:      bank,
+		Number:    number,
+	}
+
+	account.ID = uuid.NewV4().String()
+	account.CreatedAt = time.Now()
+
+	err := account.isValid()
+	if err != nil {
+		return nil, err
+	}
+
+	return &account, nil
+}
