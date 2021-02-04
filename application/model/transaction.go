ccd0006c8575b38aaa7f6e4396a26bb7fede3401
Thiago Alves <thiago_marketingdigital@hotmail.com>
2021-02-04 12:26:57 -0300

    created transaction model representation

---
+++ application/model/transaction.go
@@ -0,0 +1,43 @@
+package model
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/go-playground/validator/v10"
+)
+
+type Transaction struct {
+	ID           string  `json:"id" validate:"required,uuid64"`
+	AccountID    string  `json:"accountId" validate:"required,uuid64"`
+	Amount       float64 `json:"amount" validate:"required,numeric"`
+	PixKeyTo     string  `json:"pixKeyTo" validate:"required"`
+	PixKeyKindTo string  `json:"pixKeyKindTo" validate:"required"`
+	Description  string  `json:"description" validate:"required"`
+	Status       string  `json:"status" validate:"required"`
+	Error        string  `json:"error"`
+}
+
+func (t *Transaction) isValid() error {
+	v := validator.New()
+	err := v.Struct(t)
+	if err != nil {
+		fmt.Errorf("Error during Transaction validation: %s", err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func (t *Transaction) ParseJson(data []byte) error {
+
+	err := json.Unmarshal(data, t)
+	if err != nil {
+		return err
+	}
+
+	err = t.isValid()
+	if err != nil {
+		return err
+	}
+	return nil
+}
